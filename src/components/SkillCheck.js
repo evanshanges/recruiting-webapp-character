47262b0aa500cf9e16e8889ecfe7c49debc7b048
EvanShangES <evanshanges@gmail.com>
2023-10-06 00:15:40 -0400

    fixed tabbing styles on some files

--- src/components/SkillCheck.js
+++ src/components/SkillCheck.js
@@ -4,59 +4,59 @@ import { getModifier, getAttributeValue } from "../utils/helpers";
 
 const SkillCheck = ({ attributes, skills }) => {
   const [selectedSkill, setSelectedSkill] = useState(SKILL_LIST[0].name);
-	const [difficulty, setDifficulty] = useState(0);
-	const [rollValue, setRollValue] = useState();
-	const [totalSkillValue, setTotalSkillValue] = useState();
-	const [result, setResult] = useState()
+  const [difficulty, setDifficulty] = useState(0);
+  const [rollValue, setRollValue] = useState();
+  const [totalSkillValue, setTotalSkillValue] = useState();
+  const [result, setResult] = useState()
 
-	useEffect(() =>{
-		setResult(null)
-	}, [skills])
+  useEffect(() =>{
+    setResult(null)
+  }, [skills])
 
-	const checkResult = () => {
-		const randomValue = Math.floor(Math.random() * 20) + 1;
-		setRollValue(randomValue);
+  const checkResult = () => {
+    const randomValue = Math.floor(Math.random() * 20) + 1;
+    setRollValue(randomValue);
 
-		const characterSkill = skills.find((skill) => skill.name === selectedSkill);
-		const skillValue = characterSkill.value;
-		const attributeValue = getAttributeValue(attributes, characterSkill.attributeModifier);
-		const modifierValue = getModifier(attributeValue);
+    const characterSkill = skills.find((skill) => skill.name === selectedSkill);
+    const skillValue = characterSkill.value;
+    const attributeValue = getAttributeValue(attributes, characterSkill.attributeModifier);
+    const modifierValue = getModifier(attributeValue);
 
-		setTotalSkillValue(skillValue + modifierValue);
-		setResult((skillValue + modifierValue + randomValue) >= difficulty);
-	}
+    setTotalSkillValue(skillValue + modifierValue);
+    setResult((skillValue + modifierValue + randomValue) >= difficulty);
+  }
 
   return (
     <>
       <div>
         <h3>SkillCheck</h3>
-				<span>
-					<span>Skill: &nbsp;</span>
-					<select onChange={(e) => setSelectedSkill(e.target.value)}>
-						{SKILL_LIST.map((skill) => (
-							<option key={skill.name}>{skill.name}</option>
-						))}
-					</select>
-					<span>&nbsp;DC:&nbsp;</span>
-					<input
-						type='number'
-						size='10'
-						value={difficulty}
-						onChange={(e) => setDifficulty(e.target.value)}
-					/>
-      		<button onClick={checkResult}>Roll</button>
-				</span>
+        <span>
+          <span>Skill: &nbsp;</span>
+          <select onChange={(e) => setSelectedSkill(e.target.value)}>
+            {SKILL_LIST.map((skill) => (
+              <option key={skill.name}>{skill.name}</option>
+            ))}
+          </select>
+          <span>&nbsp;DC:&nbsp;</span>
+          <input
+            type='number'
+            size='10'
+            value={difficulty}
+            onChange={(e) => setDifficulty(e.target.value)}
+          />
+          <button onClick={checkResult}>Roll</button>
+        </span>
       </div>
-			{result !== null && (
-				<div style={{marginTop: 20}}>
-					<span>Skill: {selectedSkill}: {totalSkillValue}</span><br />
-					<span>Dice Roll: {rollValue}</span><br />
-					<span>DC Value: {difficulty}</span><br />
-					<span>Result: <b>{result ? 'Success' : 'Failure' }</b></span>
-				</div>
-			)}
-			
+      {result !== null && (
+        <div style={{marginTop: 20}}>
+          <span>Skill: {selectedSkill}: {totalSkillValue}</span><br />
+          <span>Dice Roll: {rollValue}</span><br />
+          <span>DC Value: {difficulty}</span><br />
+          <span>Result: <b>{result ? 'Success' : 'Failure' }</b></span>
+        </div>
+      )}
     </> 
   );
 }
+
 export default SkillCheck;
